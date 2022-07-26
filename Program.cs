04d089d64c807893362672887a737a85abcc0e25
NikolayCheremnov <coolnick1999@gmail.com>
2022-07-27 00:21:53 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Введите радиус: ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            int count = 2;
+            for (int i = 0; i < 2 * r; i++)
+            {
+                
+                for (int j = r; j >= 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < count; j++)
+                {
+                    Console.Write("*");
+                }
+                count++;
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Hello, GitHub!");
+        }
+    }
+}
